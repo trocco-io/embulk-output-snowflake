527b8678d2a453970085c13beadee787ccf523a8
giwa <ugw.gi.world@gmail.com>
2020-07-08 16:41:37 +0900

    create stage identifier holder

---
+++ src/main/java/org/embulk/output/snowflake/StageIdentifierHolder.java
@@ -0,0 +1,20 @@
+package org.embulk.output.snowflake;
+
+import com.google.common.base.Optional;
+import org.embulk.output.SnowflakeOutputPlugin;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StageIdentifierHolder {
+    private static final String pattern = "yyyyMMdd";
+    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+    private static final String date = simpleDateFormat.format(new Date());
+    private static final String salt = SnowflakeUtils.randomString(6);
+    private static final String snowflakeStageName = "embulk_snowflake_" + date + salt;
+    private static final String snowflakeDestPrefix = date + "_" + salt;
+
+    public static StageIdentifier getStageIdentifier(SnowflakeOutputPlugin.SnowflakePluginTask t){
+        return new StageIdentifier(t.getDatabase(), t.getSchema(), snowflakeStageName, Optional.of(snowflakeDestPrefix));
+    }
+}

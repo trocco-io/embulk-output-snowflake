d4e04bc6bdef8d008016bb652f9efc8760ee64b6
civitaspo <civitaspo@gmail.com>
2021-09-17 22:14:11 +0900

    Setup TestingEmbulk

--- src/test/java/org/embulk/output/snowflake/TestSnowflakeOutputPlugin.java
+++ src/test/java/org/embulk/output/snowflake/TestSnowflakeOutputPlugin.java
@@ -1,3 +1,26 @@
 package org.embulk.output.snowflake;
 
-public class TestSnowflakeOutputPlugin {}
+import java.util.Properties;
+import org.embulk.EmbulkSystemProperties;
+import org.embulk.input.file.LocalFileInputPlugin;
+import org.embulk.output.SnowflakeOutputPlugin;
+import org.embulk.parser.csv.CsvParserPlugin;
+import org.embulk.spi.FileInputPlugin;
+import org.embulk.spi.OutputPlugin;
+import org.embulk.spi.ParserPlugin;
+import org.embulk.test.TestingEmbulk;
+import org.junit.Rule;
+
+public class TestSnowflakeOutputPlugin {
+  private static final EmbulkSystemProperties EMBULK_SYSTEM_PROPERTIES =
+      EmbulkSystemProperties.of(new Properties());
+
+  @Rule
+  public TestingEmbulk embulk =
+      TestingEmbulk.builder()
+          .setEmbulkSystemProperties(EMBULK_SYSTEM_PROPERTIES)
+          .registerPlugin(FileInputPlugin.class, "file", LocalFileInputPlugin.class)
+          .registerPlugin(ParserPlugin.class, "csv", CsvParserPlugin.class)
+          .registerPlugin(OutputPlugin.class, "snowflake", SnowflakeOutputPlugin.class)
+          .build();
+}
